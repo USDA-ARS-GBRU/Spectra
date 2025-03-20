d7e0657b938dda76aba65d5fc4248a611ed39cb5
Andrew Ontano <andrew.ontano@gmail.com>
2025-03-20 11:18:59 -0500

    Add files via upload

---
+++ spectra.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+# this is the script to house all components
+# Core imports: argparse, importlib,  and os will be needed for all three arms
+import argparse
+import importlib
+
+def moduleFromPath(path):
+    return path[1:].replace(".py", "").replace("/", ".")
+
+
+# TODO: add configure.ini for some hard-coded values like chi-square p-value
+# TODO: modify argparse to be a universal set of inputs
+parser = argparse.ArgumentParser(description='Spectra genetic profiling. Counting, processing, and visualization of 3-mers')
+subparsers = parser.add_subparsers(title='spectra', dest='subparsers', required=True)
+
+parserCount = subparsers.add_parser("count", description="Generate tsv file of spectra counts")
+parserCount.add_argument('-i', '--input', dest='input_sequence', type=str, help='Input sequence file', required=True)
+parserCount.add_argument('-f', '--format', dest='sequence_format', type=str, help='Input file type', default='fasta')
+parserCount.add_argument('-w', '--width', dest='width', type=int, help='Window width', default='3000')
+parserCount.add_argument('-s', '--spacing', dest='spacing', type=int, help='Window spacing', default='3000')
+parserCount.add_argument('-o', '--output', dest='output', type=str, help='Output tsv file', default='spectra_report.tsv')
+parserCount.add_argument('-c', '--complement', dest='complement', action='store_true', help='Complement sequence file name. If set, calculates spectra for sequence complement (not reversed-complemented)', default=False)
+parserCount.add_argument('-l', '--libraries', dest='libraries', action='store_true', help='Sequence names include multiple libraries, prefixed by LIBRARY_', default=False)
+parserCount.add_argument('-p', '--proportions', dest='proportions', action='store_true', help='Return Spectra 3-mer proportions instead of raw counts', default=False)
+parserCount.add_argument('-m', '--memory', dest='memory', action='store_true', help='Use memory-conservation mode', default=False)
+parserCount.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
+parserCount.add_argument('-n', '--no-overlap', dest='overlap', action='store_false', help='Count base pairs in repetitive runs of nucleotides only once.', default=True)
+
+parserQuery = subparsers.add_parser("query", description="Generate tsv file of spectra counts")
+parserQuery.add_argument('-i', '--input', dest='input_sequence', type=str, help='Input sequence file', required=True)
+parserQuery.add_argument('-f', '--format', dest='sequence_format', type=str, help='Input file type', default='fasta')
+parserQuery.add_argument('-q', '--query', dest='query', type=str, help='Query sequences, separated by commas', required=True)
+parserQuery.add_argument('-w', '--width', dest='width', type=int, help='Window width', default='3000')
+parserQuery.add_argument('-s', '--spacing', dest='spacing', type=int, help='Window spacing', default='3000')
+parserQuery.add_argument('-o', '--output', dest='output', type=str, help='Output tsv file', default='spectra_report.tsv')
+parserQuery.add_argument('-l', '--libraries', dest='libraries', action='store_true', help='Sequence names include multiple libraries, prefixed by LIBRARY_', default=False)
+parserQuery.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
+parserQuery.add_argument('-m', '--memory', dest='memory', action='store_true', help='Use memory-conservation mode', default=False)
+parserQuery.add_argument('-c', '--complement', dest='complement', action='store_true', help='Complement sequence file name. If set, calculates spectra for sequence complement (not reversed-complemented)', default=False)
+parserQuery.add_argument('-n', '--no-overlap', dest='overlap', action='store_false', help='Verbose mode', default=True)
+
+parserCollate = subparsers.add_parser('collate', description='Collate multiple spectra output tsv into a multi-library tsv')
+parserCollate.add_argument('-i', '--input', dest='input_tsvs', help='Input spectra tsvs, separated by spaces', nargs='*', required=True)
+parserCollate.add_argument('-o', '--output', dest='output', help='Output spectra tsv', default='collated_spectra.tsv')
+parserCollate.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
+
+parserTransform = subparsers.add_parser('transform', description='Transform spectra data for additional insight')
+parserTransform.add_argument('-i', '--input', dest='input_tsv', type=str, help='Input spectra tsv', required=True)
+parserTransform.add_argument('-o', '--output', '--output', dest='output', type=str, help='Output spectra tsv', default=False)
+parserTransform.add_argument('-r', '--weighted-filter', dest='weighted_filter', action='store_true', help='Produce two additional outputs that have outlier windows and normal windows', default=False)
+parserTransform.add_argument('-n', '--weighted-norm', dest='weighted_normalization', action='store_true', help='Normalize spectra frequencies for each window by the frequencies for the whole sequence', default=False)
+parserTransform.add_argument('-f', '--freq', dest='frequencies', action='store_true', help='Mark this is Spectra data is already in frequencies', default=False)
+parserTransform.add_argument('-c', '--convert', dest='convert', action='store_true', help='Convert between counts and frequencies', default=False)
+parserTransform.add_argument('-s', '--window-resize', dest='resize_window', type=int, help='Resize windows to summarize N for every 1 window')
+parserTransform.add_argument('-p', '--print', dest='print', action='store_true', help='Print global frequencies', default=False)
+parserTransform.add_argument('-y', '--simplify', dest='simplify', action='store_true', help='Simplify forward and reverse-complement counts per window', default=False)
+parserTransform.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose mode', default=False)
+
+parserPlot = subparsers.add_parser('plot', description='Plot spectra profiles')
+parserPlot.add_argument('-i', '--input', dest='input_tsv', type=str, help='Input spectra tsv', required=True)
+parserPlot.add_argument('-o', '--output', dest='output', type=str, help='Output spectra plot', default='spectra_plot.png')
+parserPlot.add_argument('-z', '--zoom', dest='zoom_width', type=str, help='Plot only a portion of the windows from between X,Y')
+parserPlot.add_argument('-s', '--sequence', dest='sequence', type=str, help='Plot only sequences matching Name1,Name2,Name3')
+parserPlot.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
+
+parserAnalyze = subparsers.add_parser('analyze', description='Analyze spectra profiles')
+parserAnalyze.add_argument('-i', '--input', dest='input_tsv', type=str, help='Input spectra tsv', required=True)
+parserAnalyze.add_argument('-o', '--output', dest='output_tsv', type=str, help='Output spectra tsv', default=False)
+parserAnalyze.add_argument('-p', '--penalty', dest='penalty', type=float, help='Ruptures breakpoint penalty criterion.', default=1000000)
+parserAnalyze.add_argument('-a', '--aligned', dest='is_aligned', action='store_true', help='Check for if input tsv comes from alignment or from sequence data', default=False)
+parserAnalyze.add_argument('-s', '--size', dest='size', type=int, help='Minimum windows to be considered a novel segment', default=5)
+parserAnalyze.add_argument('-f', '--frequencies', dest='frequency', action='store_true', help='Process breaks by frequencies instead of raw counts', default=False)
+parserAnalyze.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='Verbose mode', default=False)
+
+args = parser.parse_args()
+
+subparsersScript = f"/scripts/{args.subparsers}.py"
+script = importlib.import_module(moduleFromPath(subparsersScript))
+script.execute(args)
---
+++ spectral.py
@@ -0,0 +1,117 @@
+import ruptures as rpt
+import pandas as pd
+import numpy as np
+from Bio import Seq
+
+# Shorthand for string reverse-complement using Biopython
+def rc(sequence):
+    sequence = Seq.Seq(sequence)
+    return str(sequence.reverse_complement())
+
+# Validate takes an input df, and returns a tuple of (0) approved spectra descriptor fields and (1) triplet or query names
+def validate(df):
+    columns = df.columns
+    validatedDescriptors = []
+    for descriptor in ['Library', 'Sequence', 'Block', 'Length', 'Start', 'End']:
+        if descriptor in columns:
+            validatedDescriptors.append(descriptor)
+    validatedNames = [a for a in columns if a[0] in ['A', 'C', 'G', 'T']]
+    return validatedDescriptors, validatedNames
+
+# Simplify bi-directional frequencies/counts to unidirectional
+def simplify(spectra, index=4, dim=64):
+    simpleQueries = {}
+    for query in list(spectra.columns)[index:index + dim]:
+        if query not in list(simpleQueries.values()) or rc(query) not in list(simpleQueries.keys()):
+            simpleQueries[query] = rc(query)
+    for mer in simpleQueries:
+        if simpleQueries[mer] != mer:
+            spectra[mer] += spectra[simpleQueries[mer]]
+    return spectra.drop(columns=[simpleQueries[a] for a in simpleQueries if a != simpleQueries[a]])
+
+# Counts the spectra of a sequence
+def windowCount(seq):
+    return seq[4] + [seq[2] + 1, seq[2] + len(seq[0]) if seq[2] + len(seq[0]) < seq[3] else seq[3]] + [
+               seq[0].count_overlap(a) for a in seq[1]]
+
+def windowCountNoOverlap(seq):
+    return seq[4] + [seq[2] + 1, seq[2] + len(seq[0]) if seq[2] + len(seq[0]) < seq[3] else seq[3]] + [
+        seq[0].count(a) for a in seq[1]]
+
+# Calculate breakpoints from spectra
+# For 64 literal counts, a penalty of 1,000,000 is ideal, but for frequencies a penalty of 0.5 is ideal
+def getBreakpoints(spectra, index=4, dim=64, penalty=1000000, min_size=5):
+    spectra = spectra.groupby(['Library', 'Sequence'])
+    output = []
+    # for each grouping, calculate rupture breakpoints, then convert them to starting window indices
+    for group in spectra:
+        data = group[1].iloc[0:len(group[1]), index:index + dim].to_numpy()
+        if len(data) > min_size * 2:
+            dataAlgo = rpt.KernelCPD(min_size=min_size).fit(data).predict(pen=penalty)
+            output.append((group[0], group[1].iloc[[a-1 for a in dataAlgo], 2:3]['Start'].tolist()))
+        else:
+            output.append((group[0], []))
+    return output
+
+# Use breakpoints to append as bin column to spectra
+def applyBreakpoints(spectra, breakpoints):
+    for bkp in breakpoints:
+        breakCount = 0
+        index = 1
+        breakName = ''
+        if bkp[1]:
+            for bkpStart in bkp[1]:
+                breakName = f'{bkp[0][0]}_{bkp[0][1]}_{breakCount:02d}'
+                indices = list(spectra.loc[(spectra['Library'] == bkp[0][0]) & (spectra['Sequence'] == bkp[0][1]) & (
+                        spectra['Start'] >= index) & (spectra['End'] < bkpStart)].index)
+                spectra.loc[indices, 'Block'] = breakName
+                index = bkpStart
+                breakCount += 1
+            indices = list(spectra.loc[(spectra['Library'] == bkp[0][0]) & (spectra['Sequence'] == bkp[0][1]) & (
+                        spectra['Start'] >= index)].index)
+            spectra.loc[indices, 'Block'] = breakName
+        else:
+            breakName = f'{bkp[0][0]}_{bkp[0][1]}_{breakCount:02d}'
+            indices = list(spectra.loc[(spectra['Library'] == bkp[0][0]) & (spectra['Sequence'] == bkp[0][1])].index)
+            spectra.loc[indices, 'Block'] = breakName
+    return spectra
+
+def getBreakpointFrequencies(spectra, frequency, index=4, dim=64):
+    spectra = spectra.groupby(['Block', 'Library', 'Sequence'])
+    outputs = pd.DataFrame()
+    for group in spectra:
+        frequencies = getGlobalFrequencies(group[1], frequency, index=index, dim=dim)
+        outputs.loc[len(outputs.index), ['Library', 'Sequence', 'Bin', 'Length', 'Start', 'End'] + list(frequencies.keys())] = [group[0][1], group[0][2], str(group[0][0]), str(max(group[1]['End']) - min(group[1]['Start']) + 1), str(min(group[1]['Start'])), str(max(group[1]['End']))] + list(frequencies.values())
+    return outputs
+
+# Transform spectra counts to spectra frequencies
+def countToFrequency(spectra, index=4, dim=64, merLen=3):
+    for row in spectra.iterrows():
+        denominator = row[1]['End'] - row[1]['Start'] - (merLen - 2)
+        spectra.iloc[row[0], index:index + dim] = np.array(spectra.iloc[row[0], index:index + dim]) / denominator
+    return spectra
+
+# Transform spectra counts to spectra frequencies
+def frequencyToCount(spectra, index=4, dim=64, merLen=3):
+    return spectra
+
+# Calculate global frequencies across spectra
+def getGlobalFrequencies(spectra, frequency=False, index=4, dim=64):
+    if frequency:
+        return dict(zip(spectra.columns[index:index + dim], np.array(spectra.iloc[0:len(spectra), index:index + dim].sum()/len(spectra))))
+    else:
+        counts = np.array(spectra.sum())
+        return dict(zip(spectra.columns[index:index + dim], np.divide(counts[index:index + dim], counts[3] - counts[2] - len(spectra.Library))))
+
+# Reduce frequencies from a given set of global frequencies
+def reduceFrequencies():
+    return
+
+# needs work. Currently does nothing
+def spectraRC(spectra, index=4, dim=64, merLen=3):
+    newSpectra = spectra
+    mers = list(spectra.columns)[index:index + dim]
+    for mer in mers:
+        #newSpectra.loc[rc(mer)] = spectra[mer]
+        newSpectra[rc(mer)].replace(spectra[mer])
+    return newSpectra
