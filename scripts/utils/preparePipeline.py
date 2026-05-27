40ee61945cdcd55fb858ccb4c01d309015b55a91
Andrew Ontano <andrew.ontano@gmail.com>
2026-05-27 10:17:51 -0500

    - Revisions

--- scripts/utils/preparePipeline.py
+++ scripts/utils/preparePipeline.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('-p', '--output-prefix', dest='prefix', default='spectra_pipeline', help='Output files prefix. A directory will be created with this name for storing images')
     parser.add_argument('-t', '--threads', dest='threads', type=int, help='Processing threads for multi-threading routines', required=True)
     parser.add_argument('-k', '--kmer-size', dest='mer_size', type=int, help='kmer size in query [default 20]', default=20)
-    parser.add_argument('-m', '--minimum-sequence-size', dest='minimum_size', type=int, help='Minimum sequence size to include in reports [100,000 bp]', default=100000)
+    parser.add_argument('-m', '--minimum-sequence-size', dest='minimum_size', type=int, help='Minimum sequence size to include in reports [1 bp]', default=1)
     parser.add_argument('-c', '--counter', dest='counter', choices=['jellyfish', 'meryl'], default='jellyfish', help='K-mer counter to use [default jellyfish]')
     parser.add_argument('--canonical', dest='canonical', action='store_true', help='Generate canonical spectra in addition to non-canonical', default=False)
     parser.add_argument('--n-gaps', dest='ngaps', action='store_true', help='Label gaps in the assembly in the final report', default=False)
